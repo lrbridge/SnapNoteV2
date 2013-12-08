e3235f346ee4ba608ac86d72223a542eec5d50f3
lrbridge <lrbridge@Laurens-MacBook-Pro.local>
2013-12-07 21:35:22 -0800

    Revert c2c1632..80c9adb
    This rolls back to commit c2c16328ea7a4a1241d35d1723640c190e04d07e.

--- app/www/js/deck-list.js
+++ app/www/js/deck-list.js
@@ -2,27 +2,12 @@ angular.module('snapnote').controller('DeckListCtrl',
     function ($scope, SampleDecks) {
 		
 		$scope.decks = SampleDecks.getMyDecks();
-      	$scope.photos = ["img/demoslides/whiteboard1.png"];
-        
-        // Do an ajax call to get all the files in the demoslides directory
-        // this runs each time the deck-list page is loaded
-        /*var dir = "img/demoslides";
-        var fileextension = ".png";
-        alert("before ajax");
-        $.ajax({
-            url: dir,
-            success: function (data) {
-                $(data).find("a:contains(" + fileextension + ")").each(function () {
-                    var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-                    alert(dir+"/"+filename);
-                    $scope.photos.push(dir+"/"+filename);
-                });
-                alert("apply");
-                $scope.$apply();
-            },
-            error: function(x) {
-                alert(x);   
-            }
-        });*/
+      	
+        $scope.photos = [
+            "img/demoslides/book1.png",
+            "img/demoslides/book2.png",
+            "img/demoslides/whiteboard1.png",
+            "img/demoslides/whiteboard2.png"
+        ];
         
 	});
