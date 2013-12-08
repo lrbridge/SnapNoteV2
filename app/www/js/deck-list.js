714e61f8cb90dac1f4de991ca158cc8c9c6773d9
lrbridge <lrbridge@Laurens-MacBook-Pro.local>
2013-12-07 20:24:30 -0800

    Revert "Grabbing all files from img/demoslides directory" - didn't work on emulator… might put it back later
    This reverts commit 24590ca434c24dbb758ba3097c4e6e2c342fed6a.

--- app/www/js/deck-list.js
+++ app/www/js/deck-list.js
@@ -2,21 +2,12 @@ angular.module('snapnote').controller('DeckListCtrl',
     function ($scope, SampleDecks) {
 		
 		$scope.decks = SampleDecks.getMyDecks();
-      	$scope.photos = [];
-        
-        // Do an ajax call to get all the files in the demoslides directory
-        // this runs each time the deck-list page is loaded
-        var dir = "img/demoslides";
-        var fileextension = ".png";
-        $.ajax({
-            url: dir,
-            success: function (data) {
-                $(data).find("a:contains(" + fileextension + ")").each(function () {
-                    var filename = this.href.replace(window.location.host, "").replace("http:///", "");
-                    $scope.photos.push(dir+"/"+filename);
-                });
-                $scope.$apply();
-            }
-        });
+      	
+        $scope.photos = [
+            "img/demoslides/book1.png",
+            "img/demoslides/book2.png",
+            "img/demoslides/whiteboard1.png",
+            "img/demoslides/whiteboard2.png"
+        ];
         
 	});
