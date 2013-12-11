245f59eab9954909b94097992a8c07f9a5d3d0e1
unknown <aford00@gmail.com>
2013-12-10 19:07:19 -0500

    updated the tutorials for the new views

--- app/www/js/choose-image.js
+++ app/www/js/choose-image.js
@@ -27,16 +27,30 @@ angular.module('snapnote').controller('ChooseImageCtrl',
 			var img = document.getElementById('createNoteTutorialImg');
 			if($scope.tutorialCount == 0) {
 				img.src = "img/create-tutorial-use-existing.png";
-				$("h4.createNoteBodyLabel").text("Tap an existing image to use it in the new note.");
-				var nextBtn = document.getElementById('createNoteTutorialBtn');
-				nextBtn.innerHTML = 'Done';
+				$("h4.createNoteBodyLabel").text("Tap an existing image to use one previously captured as the base of a new note.");
 			}
-			/*else if($scope.tutorialCount == 1){
-				img.src = "img/study-tutorial-options.png";
-				$("h4.studyBodyLabel").text("Tap 'Options' to add a reminder for this deck or to delete this deck.");
+			else if($scope.tutorialCount == 1){
+				img.src = "img/create-tutorial-options.png";
+				$("h4.studyBodyLabel").text("Tap 'Options' to use the undo or redo commands.");
+			}
+			else if($scope.tutorialCount == 2){
+				img.src = "img/create-tutorial-save.png";
+				$("h4.studyBodyLabel").text("Tap 'Save' to save the new note.");
+			}
+			else if($scope.tutorialCount == 3){
+				img.src = "img/create-tutorial-existing-deck.png";
+				$("h4.studyBodyLabel").text("Use 'Select Existing Deck' to save the new note to an existing deck.");
+			}
+			else if($scope.tutorialCount == 4){
+				img.src = "img/create-tutorial-existing-deck.png";
+				$("h4.studyBodyLabel").text("Use 'Create New Deck' to save the new note to a new deck.");
+			}
+			else if($scope.tutorialCount == 5){
+				img.src = "img/create-tutorial-add-another.png";
+				$("h4.studyBodyLabel").text("Tap 'Yes' or 'No' to create another note for the current deck or return to the homescreen, respectively.");
 				var nextBtn = document.getElementById('nextStudyTutorialBtn');
 				nextBtn.innerHTML = 'Done';
-			}*/
+			}
 			else {
 				$('#createNoteModal').modal('hide');
 				$scope.tutorialCount = -1;
